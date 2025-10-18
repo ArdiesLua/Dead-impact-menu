fea0ff15611819256d858697b990b41b00d42de9
ArdiesLua <ardiesardies16@gmail.com>
2025-10-19 00:35:13 +0700

    Update Dead_impact.lua

--- Dead_impact.lua
+++ Dead_impact.lua
@@ -1,12 +1,12 @@
 local libNameSo = "libil2cpp.so" -- Set Your Libname 
 local ON,OFF = "[ ON ]","[ OFF ]" -- Feature ON / OFF
+local info = gg.getTargetInfo() -- Get information about the target app
 local APK = info.label -- Get APK name
 local H1,H2,H3 = OFF,OFF,OFF -- Toggle 
 local H4,H5,H6 = OFF,OFF,OFF -- Toggle
 local H7,H8,H9 = OFF,OFF,OFF -- Toggle 
 local Title = "━─━─[ " .. APK .. " ]─━─━" -- Menu Title 
-local ti = gg.getTargetInfo()
-local APK = ti.label or "Unknown"
+
 
 -- @Ardies77 
 -- Define tables and flags for memory handling
